990c7820965cc70bd9493fba7799b8d8ebfae531
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-15 13:51:32 -0500

    basic spanish to english

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# Getting Started with Create React App
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Example of how to set up Internationalization
+## (e.g. switching from EN to ES)
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
+import English from './lang/en.json';
+import Spanish from './lang/es.json';
 import './App.css';
+
+const locale = navigator.language || 'en';
+
+const lang = (locale === 'es') ? Spanish : English;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{lang['app.header']}</h1>
+        <p>{lang['app.subhead']}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {lang['app.cta']}
         </a>
       </header>
     </div>
